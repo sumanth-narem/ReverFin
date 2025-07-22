13c5696f1ce8d80cfecc8dd0163bb4aaa58366d1
Sumanth Narem <sumanth.narem@jsspro.com>
2025-07-22 12:48:15 +0530

    Added scroll animations

--- src/components/GettingStarted/Section.tsx
+++ src/components/GettingStarted/Section.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { act, useEffect, useRef, useState } from 'react'
 import Hidden from './Hidden'
 import Button from '../ui/Button'
 import { ArrowRight } from 'lucide-react'
@@ -21,50 +21,84 @@ const steps = [
 ]
 
 function Header() {
-  const [openIndex, setOpenIndex] = useState(0) // initially first item is open
 
-  const handleToggle = (index: number) => {
-    setOpenIndex(prev => (prev === index ? 0 : index))
-  }
+  const [activeTab, setActiveTab] = useState<number>(0);
+  const [scrollValue, setScrollValue] = useState<number>(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current) {
+        const rect = containerRef.current.getBoundingClientRect();
+        const offset = window.innerHeight - rect.top;
+        setScrollValue(offset);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollValue > 700 && scrollValue < 1400) {
+      if (activeTab !== 1) {
+        setActiveTab(1);
+      }
+    } else if (scrollValue > 1400) {
+      if (activeTab !== 2) {
+        setActiveTab(2);
+      }
+    } else {
+      if (activeTab !== 0) {
+        setActiveTab(0);
+      }
+    }
+  }, [scrollValue, activeTab]);
 
   return (
     <>
-      <div className='flex items-center justify-center flex-col w-full'>
-        <div className='border-t border-primary-50 w-full max-w-[100rem] bg-primary-100 px-12'>
-          <div className='border-x border-primary-50 py-20 h-full w-full flex items-start justify-center flex-row overflow-hidden'>
-            <div className='basis-1/2 px-10 overflow-y-auto'>
-              {steps.map((step, index) => (
-                <div
-                  key={index}
-                  className={`font-ppMori flex items-start justify-start flex-col border-t ${openIndex === index ? "border-primary-500" : "border-primary-50"} duration-500 transition-colors py-3`}
-                >
-                  <div className='flex items-center justify-start gap-5 cursor-pointer w-full' onClick={() => handleToggle(index)}>
-                    <div className='flex items-center justify-center bg-primary-800 w-10 h-10 rounded-full pt-1 text-white'>
-                      {step.number}
+      <div ref={containerRef} className='h-[2100px]'>
+        <div className='sticky top-0 flex items-center justify-center flex-col w-full'>
+          <div className='border-t border-primary-50 w-full max-w-[100rem] bg-primary-100 px-12'>
+            <div className='border-x border-primary-50 py-20 h-full w-full flex items-start justify-center flex-row overflow-hidden'>
+              <div className='basis-1/2 px-10 overflow-y-auto'>
+                {steps.map((step, index) => (
+                  <div
+                    key={index}
+                    className={`font-ppMori flex items-start justify-start flex-col border-t ${activeTab === index ? "border-primary-500" : "border-primary-50"} duration-500 transition-colors py-3`}
+                  >
+                    <div className='flex items-center justify-start gap-5 cursor-pointer w-full'>
+                      <div className='flex items-center justify-center bg-primary-800 w-10 h-10 rounded-full pt-1 text-white'>
+                        {step.number}
+                      </div>
+                      <p className='text-primary-800 pt-1'>{step.title}</p>
                     </div>
-                    <p className='text-primary-800 pt-1'>{step.title}</p>
+                    <div className={`overflow-y-hidden ${activeTab === index ? "h-[22rem]" : "h-0"} min-h-0 transition-all duration-500`}>
+                      <Hidden index={index} />
+                    </div>
+                    {/* {openIndex === index && <Hidden />} */}
                   </div>
-                  <div className={`overflow-y-hidden ${openIndex === index ? "h-[22rem]" : "h-0"} min-h-0 transition-all duration-500`}>
-                    <Hidden index={index}/>
+                ))}
+              </div>
+              <div className='basis-1/2 text-primary-800 px-16 flex items-start justify-start'>
+                <div className='flex items-start justify-start flex-col gap-6'>
+                  <p data-aos="fade-up" data-aos-delay="200" className='font-alliance text-4xl'>Getting Started Is Simple</p>
+                  <p data-aos="fade-up" data-aos-delay="400" className='font-ppMori text-primary-600'>
+                    How it works text non consequatur hic dolor est unde reiciendis
+                    laboriosam cum. Sunt at repellendus reiciendis possimus.
+                  </p>
+                  <div data-aos="fade-up" data-aos-delay="600">
+                    <Button
+                      title='Get a demo'
+                      icon={<ArrowRight width={14} />}
+                      link='#'
+                      className='bg-primary-800 text-white px-5 pt-0.5 h-[2.5rem] rounded-full flex items-center justify-center'
+                    />
                   </div>
-                  {/* {openIndex === index && <Hidden />} */}
-                </div>
-              ))}
-            </div>
-            <div className='basis-1/2 text-primary-800 px-16 flex items-start justify-start'>
-              <div className='flex items-start justify-start flex-col gap-6'>
-                <p data-aos="fade-up" data-aos-delay="200" className='font-alliance text-4xl'>Getting Started Is Simple</p>
-                <p data-aos="fade-up" data-aos-delay="400" className='font-ppMori text-primary-600'>
-                  How it works text non consequatur hic dolor est unde reiciendis
-                  laboriosam cum. Sunt at repellendus reiciendis possimus.
-                </p>
-                <div data-aos="fade-up" data-aos-delay="600">
-                  <Button
-                    title='Get a demo'
-                    icon={<ArrowRight width={14} />}
-                    link='#'
-                    className='bg-primary-800 text-white px-5 pt-0.5 h-[2.5rem] rounded-full flex items-center justify-center'
-                  />
                 </div>
               </div>
             </div>
--- src/components/RoleByRole/Section.tsx
+++ src/components/RoleByRole/Section.tsx
@@ -1,52 +1,89 @@
 "use client"
 
 import { RbyRData } from '@/constants/RbyRData'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ImageComponent from '../ui/Image'
 
 function Section() {
-  const [openIndex, setOpenIndex] = useState(0);
+  const [activeTab, setActiveTab] = useState<number>(0);
+  const [scrollValue, setScrollValue] = useState<number>(0);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-  const handleToggle = (index: number) => {
-    setOpenIndex(prev => (prev === index ? 0 : index))
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current) {
+        const rect = containerRef.current.getBoundingClientRect();
+        const offset = window.innerHeight - rect.top;
+        setScrollValue(offset);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollValue > 700 && scrollValue < 1400) {
+      if (activeTab !== 1) {
+        setActiveTab(1);
+      }
+    } else if (scrollValue > 1400 && scrollValue < 2100) {
+      if (activeTab !== 2) {
+        setActiveTab(2);
+      }
+    } else if (scrollValue > 2100) {
+      if (activeTab !== 3) {
+        setActiveTab(3);
+      }
+    } else {
+      if (activeTab !== 0) {
+        setActiveTab(0);
+      }
+    }
+  }, [scrollValue, activeTab]);
 
   return (
     <>
-      <div className='flex items-center justify-center flex-wrap border-r border-primary-500'>
-        <div className='py-36 border-r border-primary-500 flex items-start justify-center flex-col basis-1/2 pl-12 w-full'>
-          <p className='font-alliance text-4xl'>What You Get - Role by Role</p>
-          <div className='w-full pt-16'>
-            {
-              RbyRData?.map((item, key) => (
-                <div key={key} className={`border-b ${openIndex === key ? "border-secondary" : "border-primary-500"} duration-500 transition-colors`}>
-                  <p className='font-ppMori text-xl py-5 w-full cursor-pointer' onClick={() => handleToggle(key)}>{item?.heading}</p>
-                  <div className={`overflow-y-hidden ${openIndex === key ? "h-[6rem]" : "h-0"} min-h-0 transition-all duration-500`}>
-                    {
-                      <ul className='text-primary-400 pb-5'>
-                        {
-                          item?.content?.map((value, key) => (
-                            <li key={key}>
-                              {value}
-                            </li>
-                          ))
-                        }
-                      </ul>
-                    }
+      <div ref={containerRef} className='h-[2600px]'>
+        <div className='sticky -top-5 flex items-center justify-center flex-wrap border-r border-primary-500'>
+          <div className='py-36 border-r border-primary-500 flex items-start justify-center flex-col basis-1/2 pl-12 w-full'>
+            <p className='font-alliance text-4xl'>What You Get - Role by Role</p>
+            <div className='w-full pt-7'>
+              {
+                RbyRData?.map((item, key) => (
+                  <div key={key} className={`border-b ${activeTab === key ? "border-secondary" : "border-primary-500"} duration-500 transition-colors`}>
+                    <p className='font-ppMori text-xl py-5 w-full cursor-pointer'>{item?.heading}</p>
+                    <div className={`overflow-y-hidden ${activeTab === key ? "h-[6rem]" : "h-0"} min-h-0 transition-all duration-500`}>
+                      {
+                        <ul className='text-primary-400 pb-5'>
+                          {
+                            item?.content?.map((value, key) => (
+                              <li key={key}>
+                                {value}
+                              </li>
+                            ))
+                          }
+                        </ul>
+                      }
+                    </div>
                   </div>
-                </div>
-              ))
-            }
+                ))
+              }
+            </div>
           </div>
-        </div>
-        <div className='basis-1/2 flex items-center justify-center'>
-          <ImageComponent
-            alt='RbyRImage'
-            loading='lazy'
-            fill
-            src='/assets/RbyR.jpg'
-            className='relative h-[30rem] w-[30rem] rounded-2xl overflow-hidden'
-          />
+          <div className='basis-1/2 flex items-center justify-center'>
+            <ImageComponent
+              alt='RbyRImage'
+              loading='lazy'
+              fill
+              src='/assets/RbyR.jpg'
+              className='relative h-[30rem] w-[30rem] rounded-2xl overflow-hidden'
+            />
+          </div>
         </div>
       </div>
     </>
